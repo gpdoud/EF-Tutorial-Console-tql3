8cfc41308c10e49b25095d044604117b070c46ed
Greg Doud <gpdoud@gmail.com>
2024-06-24 13:44:45 -0400

    Add project files.

---
+++ EF-Tutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Tutorial;
+
+internal class Program {
+
+    static void Main(string[] args) {
+
+        var context = new AppDbContext();
+
+        var requests = context.Requests.Include(x => x.User).ToList();
+
+        requests.ForEach(x => Console.WriteLine($"{x.Description} | {x.User!.Username}"));
+
+    }
+}
